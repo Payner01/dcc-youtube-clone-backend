6322222ac0f710088b4cfbe233e3c0636c04373e
andyrod <andyrod209@gmail.com>
2022-02-22 23:02:15 -0800

    Created a GET and POST View Functions + Added empty URL path tocreat_post function

--- drf_jwt_backend/comments/urls.py
+++ drf_jwt_backend/comments/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from comments import views
 
 urlpatterns = [
-    path('comment/', views.CommentList.as_view()) 
+    # path('comment/', views.CommentList.as_view()) 
+    path('comment/', views.get_all_comments),
+    path('', views.create_post),
 ]
--- drf_jwt_backend/comments/views.py
+++ drf_jwt_backend/comments/views.py
@@ -6,14 +6,36 @@ from rest_framework.decorators import api_view, permission_classes
 from .models import Comment
 from .serializers import CommentSerializer
 from django.contrib.auth.models import User
+# from drf_jwt_backend.comments import serializers
 
+# View Function
 
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_comments(request):
+    comments = Comment.objects.all()
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_post(request):
+    if request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 # Create your views here.
-class CommentList(APIView):
 
-    permission_classes = [AllowAny]
 
-    def get(self, request):
-        comment = Comment.objects.all()
-        serializer = CommentSerializer(comment, many=True)
-        return Response(serializer.data)
+
+# class CommentList(APIView):
+
+#     permission_classes = [AllowAny]
+
+#     def get(self, request):
+#         comment = Comment.objects.all()
+#         serializer = CommentSerializer(comment, many=True)
+#         return Response(serializer.data)
+
