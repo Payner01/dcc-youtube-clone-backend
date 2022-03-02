4e900fae348188579beb1a02ec81623d90e65a73
andyrod <andyrod209@gmail.com>
2022-03-02 12:59:43 -0800

    Added users to commentSerializer

--- drf_jwt_backend/comments/serializers.py
+++ drf_jwt_backend/comments/serializers.py
@@ -1,12 +1,20 @@
+from dataclasses import field
 from rest_framework import serializers
 from .models import Reply, Comment
+from django.contrib.auth.models import User
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False, read_only = True)
     class Meta:
         model = Comment
         fields = ['id', 'user', 'video_id', 'text', 'likes', 'dislikes']
 
 class ReplySerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False, read_only = True)
     class Meta:
         model = Reply
         fields = '__all__' 
